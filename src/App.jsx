7ea5154ef4b0582bb30d3cefe1d7a4dae56b28f5
MAHESH <mahesh@MAHESHs-MacBook-Air.local>
2026-02-03 23:48:35 +0530

    deploy tictactoe

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TicTacToe from "./Components/TicTacToe/TicTacToe";
+
+function App() {
+  return (
+    <>
+      <div>
+        <TicTacToe />
+      </div>
+    </>
+  );
+}
+
+export default App;
